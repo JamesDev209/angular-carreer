517e3ef0215dcaa4c0a3e2bdde32e9d69be5ead2
JamesDev209 <james1913_@hotmail.com>
2025-07-18 00:32:33 -0500

    creamos un array con 2 atributos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {Test } from './test/test'
+import {Test } from './test/test';
+import { Header } from './components/header/header';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Test],
+  imports: [RouterOutlet, Test, Header, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'aplicacion';
+  nombres: string[] = ['Dani', 'fofi']
 }
