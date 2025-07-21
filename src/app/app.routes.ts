6b84457e6196d66b8be16aa96c700a0282c1cb1d
JamesDev209 <james1913_@hotmail.com>
2025-07-21 13:12:05 -0500

    Se creo un array de rutas para  HOME- MOVIES-BLOG

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { MovieList } from './pages/movie-list/movie-list';
+import { Home } from './pages/home/home';
+import { Blog } from './components/blog/blog';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home},
+    { path: 'movies', component: MovieList},
+    { path: 'blog', component: Blog},
+
+    
+];
