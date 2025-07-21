7a4f5c5767221a646f37ea1641e4be64117fa967
JamesDev209 <james1913_@hotmail.com>
2025-07-21 13:15:07 -0500

    Se genero un array de usuarios

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,13 +3,27 @@ import { RouterOutlet } from '@angular/router';
 import {Test } from './test/test';
 import { Header } from './components/header/header';
 import { FormsModule } from '@angular/forms';
+import { Formularios } from './components/formularios/formularios';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Test, Header, FormsModule],
+  imports: [RouterOutlet, Test, Header, FormsModule, Formularios],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   nombres: string[] = ['Dani', 'fofi']
+
+    jugadores: any = [
+      {
+        id:1,
+        name:"garza",
+        edad:26
+      },
+      {
+        id:2,
+        name:"mimi",
+        edad:23
+      }
+    ]
 }
