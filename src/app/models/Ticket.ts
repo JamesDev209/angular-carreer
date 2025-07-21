0e0b24ca64c1666402c226ed2a65e2ebd93f512b
JamesDev209 <james1913_@hotmail.com>
2025-07-21 13:15:30 -0500

    Se creo la inteface del objeto

---
+++ src/app/models/Ticket.ts
@@ -0,0 +1,9 @@
+
+
+
+// Ticket Model -> define como lucira un ticket o otro objeto de cualquiero interfaz
+export default interface Ticket {
+  id: number;
+  title: string;
+  description: string;
+}
